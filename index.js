963a4ffb4ed039dba04e8e893bb0c14cdee08e61
CasperYonel <>
2021-10-12 15:29:44 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+// // Given the starting code of:
+
+// // x = 1
+// // y = 2
+// // Write a block of code that will print out the largest value.
+
+// // Usnig the given values as an example, your code should print out 2.
+
+// // To verify your code is working properly, try and change the values of x and y.
+
+// // As an additional challenge, print out The values are identical. if x and y have the same value.
+
+const x = 15;
+const y = 12;
+
+if(x > y) {
+  console.log(x);
+} else if ( y > x) {
+  console.log(y);
+} else if (x === y) {
+  (console.log("They're equal"))
+} 
